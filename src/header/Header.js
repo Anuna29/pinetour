fedf2e560aafa698eb25890c3c51dda7f80c9f03
Anuna29 <anusukh29@gmail.com>
2024-10-24 23:48:55 -0500

    started pinetour

---
+++ src/header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.css";
+import { HeaderLogo } from "../../assets"
+
+export const Header = () => {
+  return <div>
+    <HeaderLogo id="header-container" />
+  </div>
+}
+
