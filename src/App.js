8237b0ec0cd4aefd907480348350e183be3eb765
Anuna29 <anusukh29@gmail.com>
2024-10-29 18:09:15 -0500

    added destination section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import { Header, Hero } from "./components";
+import { Destination, Header, Hero } from "./components";
 
 export function App() {
   return (
     <div>
      <Header />
      <Hero />
+     <Destination />
     </div>
   );
 };
+
 
---
+++ src/components/destination/Destination.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./Destination.css"
+import { destinations } from './Data'
+
+export const Destination = () => {
+  return (
+    <div id='destination-container'>
+      <div id='destination-left'>
+        <div id='destination-subtitle'>Tours | 6 DAYS</div>
+        <div id='destination-title'>Destination of
+        Our Agency</div>
+        <div id='destination-map'>
+          {destinations.map((destination) => (
+            <div id='destination-item-container'>
+              <div id='destination-item-left'>D{destination.number}</div>
+              <div id='destination-item-right'>
+                <div id='destination-item-right-title'>{destination.title}</div>
+                <div id='destination-item-right-location'>{destination.location}</div>
+  
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div id='destination-right'>
+        <img src="images/image-5.png" alt="destination"/>
+      </div>
+    </div>
+  )
+}
